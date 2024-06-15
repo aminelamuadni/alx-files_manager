c6e014fd3a00886d46570ab77c93f2c0367c0e6f
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-15 22:12:14 +0100

    Update AppController.test.js file

--- tests/controllers/AppController.test.js
+++ tests/controllers/AppController.test.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('app Controller', () => {
-  it('gET /status should return the status of Redis and DB', () => new Promise((done) => {
+  it('GET /status should return the status of Redis and DB', () => new Promise((done) => {
     chai.request(app)
       .get('/status')
       .end((_err, res) => {
@@ -17,7 +17,7 @@ describe('app Controller', () => {
       });
   }));
 
-  it('gET /stats should return the stats of users and files', () => new Promise((done) => {
+  it('GET /stats should return the stats of users and files', () => new Promise((done) => {
     chai.request(app)
       .get('/stats')
       .end((_err, res) => {
