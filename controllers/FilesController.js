284e454583c47fa00f500f2d4ff3ffbdb54477ab
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-15 21:18:39 +0100

    Update FilesController.js file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -88,7 +88,7 @@ class FilesController {
     }
 
     const parentId = req.query.parentId || '0';
-    const page = parseInt(req.query.page || '0');
+    const page = parseInt(req.query.page || '0', 10);
     const limit = 20;
 
     const files = await dbClient.db.collection('files').find({
@@ -98,6 +98,48 @@ class FilesController {
       .toArray();
 
     return res.status(200).json(files);
+  }
+
+  static async putPublish(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const file = await dbClient.db.collection('files').findOneAndUpdate(
+      { _id: new ObjectId(req.params.id), userId: new ObjectId(userId) },
+      { $set: { isPublic: true } },
+      { returnOriginal: false },
+    );
+
+    if (!file.value) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    return res.status(200).json(file.value);
+  }
+
+  static async putUnpublish(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const file = await dbClient.db.collection('files').findOneAndUpdate(
+      { _id: new ObjectId(req.params.id), userId: new ObjectId(userId) },
+      { $set: { isPublic: false } },
+      { returnOriginal: false },
+    );
+
+    if (!file.value) {
+      return res.status(404).json({ error: 'Not found' });
+    }
+
+    return res.status(200).json(file.value);
   }
 }
 
