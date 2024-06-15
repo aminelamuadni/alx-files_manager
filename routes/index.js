0de4020fbe0b0dda2b243c09cba69ce97c355638
Amine Lamuadni <W7equwE5@gmail.com>
2024-06-15 20:46:36 +0100

    Update index.js file

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 // Define routes
 router.get('/status', AppController.getStatus);
@@ -10,5 +11,10 @@ router.get('/stats', AppController.getStats);
 
 // New user registration route
 router.post('/users', UsersController.postNew);
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
